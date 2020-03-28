88bbfd76d0545aa3d716bdabfbcd330941c3809b
Akashdeep Singh <Akash>
2020-03-27 17:18:31 -0700

    now writing location to file

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -26,7 +30,13 @@ class _HomePageState extends State<HomePage> {
               child: Text("Get location"),
               onPressed: () {
                 // Get location here
+                log("Write my location to file");
                 _getCurrentLocation();
+                _writeMyLocationToFile(_currentPosition.toString());
+                _readMyLocationFromFile().then((location){
+                  log("Written location to file"+ location);
+                });
+
               },
             ),
           ],
@@ -47,5 +57,38 @@ class _HomePageState extends State<HomePage> {
      }).catchError((e) {
        print(e);
      });
+   }
+
+
+
+
+   Future<String> get _localPath async {
+     final directory = await getApplicationDocumentsDirectory();
+     return directory.path;
+   }
+
+   Future<File> get _localFile async {
+     final path = await _localPath;
+     return File('$path/mylocation.txt');
+   }
+
+   Future<File> _writeMyLocationToFile(String locationString) async {
+     final file = await _localFile;
+     // Write the file.
+     return file.writeAsString('$locationString');
+   }
+
+   Future<String> _readMyLocationFromFile() async {
+     try {
+       final file = await _localFile;
+
+       // Read the file.
+       String contents = await file.readAsString();
+
+       return contents;
+     } catch (e) {
+       // If encountering an error, return 0.
+       return null;
+     }
    }
 }
